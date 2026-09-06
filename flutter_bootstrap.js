271703dd1d48bc8f07a2e3f3d0c1d96be91f92f0
stevekissy <genspark_flutter_dev@genspark.ai>
2026-09-06 16:05:04 +0000

    deploy: connexion Super Admin uniquement — Membre équipe supprimé

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3533117627"
+    serviceWorkerVersion: "2498377296"
   }
 });
